98082ffcdb9ecc051710dc9e5f9f39465a6c9087
lilly <lilly1987@naver.com>
2018-09-03 20:30:39 +0900

    스택으로 구현

---
+++ 약수/약수.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <memory.h>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	// 입력값
+	unsigned int n,c,m1,m2;
+	cout << "찿을값:";
+	cin >> n;
+	cout << endl;
+
+	// 만일 n이 1보다 작거나 같으면 함수 종료
+	if (n <= 1) {
+		cout << "1보다 작음";
+		cout << endl;
+		return 0;
+	}
+
+	// 스택 생성
+	stack<int> s;
+
+	c = 0;
+
+	while (true)
+	{
+		c += 1;
+		if (c<=n)
+		{
+			m1 = int(n / c);
+			m2 = n % c;
+			if (m2==0)
+			{
+				s.push(c);
+				cout << s.size() << ":" << c << endl;
+			}
+		}
+		else {
+			cout << endl;
+			cout << s.size()<<"개"<< endl;
+			cout << endl;
+			c = 0;
+			//while(0< s.size()) //성능은 어느쪽이 좋을려나
+			while(!s.empty())
+			{
+				cout << ++c << ":"<< s.top() << endl;
+				s.pop();
+			}
+			cout << endl;
+			return 0;
+		}
+	}
+
+
+	return 0;
+}
+
